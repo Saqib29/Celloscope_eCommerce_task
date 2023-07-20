126bac70b32c95f4410e5aa488a6d7a56561d5f7
saqib <saqib29111@gmail.com>
2023-07-20 22:13:42 +0600

    updtae logger showing strategy

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ dotenv.config()
 // Connection url 
 const sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI, {
     dialect: 'postgres', 
+    logging: process.env.NODE_ENV == 'development' ? true : false
 })
 
 
